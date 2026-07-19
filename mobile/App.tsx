a2cbafb0ad52303a72cd37f5c28fe10c2c812a4e
Keerthana Gopinath <Keerthanag@ALHAJERY.COM.KW>
2026-07-19 12:36:47 +0300

    Update App.tsx

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,12 +1,18 @@
+import { AuthProvider } from '@/auth/AuthContext';
 import React from 'react';
 import { Text } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 
 export default function App() {
   return (
-  <GestureHandlerRootView style={{ flex: 1 }}>
-  <Text>Hello</Text>
+ <GestureHandlerRootView style={{ flex: 1 }}>
+  <SafeAreaProvider>
+    <AuthProvider>
+      <Text>Hello</Text>
+    </AuthProvider>
+  </SafeAreaProvider>
 </GestureHandlerRootView>
 );
 }
