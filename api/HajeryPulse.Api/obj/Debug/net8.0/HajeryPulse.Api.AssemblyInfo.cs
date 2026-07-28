4b7e22013665d03c05bae3dabfc3c964b2a1bd17
Keerthana Gopinath <Keerthanag@ALHAJERY.COM.KW>
2026-07-28 13:26:18 +0300

    React version change

--- api/HajeryPulse.Api/obj/Debug/net8.0/HajeryPulse.Api.AssemblyInfo.cs
+++ api/HajeryPulse.Api/obj/Debug/net8.0/HajeryPulse.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HajeryPulse.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37b1e8a7f78584c10d15ea623dab6bc3fc0c4562")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2db121793f00770a3e89382530f33389d0d1a51")]
 [assembly: System.Reflection.AssemblyProductAttribute("HajeryPulse.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HajeryPulse.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
